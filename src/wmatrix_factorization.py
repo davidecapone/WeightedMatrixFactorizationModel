aa4f6b3716cea026d6d4e2ef5dcf6f112173a984
Davide Capone <davidecapone2304@gmail.com>
2024-01-26 13:46:25 +0100

    minor changes

--- src/wmatrix_factorization.py
+++ src/wmatrix_factorization.py
@@ -5,16 +5,25 @@ class WeightedMatrixFactorization():
   Weighted Matrix Factorization class.
   """
 
-  def __init__(self, feedback_matrix, n_latents=100, n_iter=20) -> None:
+  def __init__(self, feedback_matrix, 
+               n_latents:int=100, 
+               n_iter:int=20, 
+               w_obs:float=1.0, 
+               w_unobs:float=0.1) -> None:
     """
     Initialize the Weighted Matrix Factorization.
     """
     self.feedback_matrix = feedback_matrix
-    self.n_latents = n_latents
-    self.n_iter = n_iter
 
     # get the number of users and items:
     self.n_users, self.n_items = self.feedback_matrix.shape
+
+    self.n_iter = n_iter
+    self.n_latents = n_latents
+
+    # weights for observed and unobserved values:
+    self.w_obs = w_obs
+    self.w_unobs = w_unobs
 
     # filling random values in the two matrices:
     self.user_matrix = np.random.rand(self.n_users, self.n_latents)
@@ -32,13 +41,49 @@ class WeightedMatrixFactorization():
       None
     """
 
-    if method == 'WALS':
-      pass
-    else:
-      pass
+    match method:
+      case 'WALS':
+        pass
+      case _:
+        raise NotImplementedError(f"Method {method} not yet implemented.")
 
     return
 
+  def update_user_matrix(self):
+    """
+    Update the user matrix.
+
+    Args:
+      None
+
+    Returns:
+      None
+    """
+    pass
+
+  def update_item_matrix(self):
+    """
+    Update the item matrix.
+
+    Args:
+      None
+
+    Returns:
+      None
+    """
+    pass
+
+  def loss(self):
+    """
+    Compute the loss for the current iteration.
+
+    Args:
+      None
+
+    Returns:
+      None
+    """
+    pass
 
   def predict(self):
     """
