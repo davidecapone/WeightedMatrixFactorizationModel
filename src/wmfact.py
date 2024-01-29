18cd16935c73182d7895c56e2dcc5c00b69b5d31
Davide Capone <davidecapone2304@gmail.com>
2024-01-29 10:18:05 +0100

    code refactoring #2

--- src/wmfact.py
+++ src/wmfact.py
@@ -1,10 +1,11 @@
 import numpy as np
 from numpy.linalg import solve
 import time
+import pickle
 
 class WeightedMatrixFactorization():
 
-  def __init__(self, feedback_matrix, 
+  def __init__(self, feedbacks:np.ndarray, 
                seed:int=None,
                n_latents:int=100, 
                n_iter:int=20, 
@@ -12,30 +13,37 @@ class WeightedMatrixFactorization():
                w_unobs:float=0.1,
                lambda_reg:float=0.05) -> None:
     
-    self.feedback_matrix = np.nan_to_num(np.array(feedback_matrix), 0) # Replace NaN values with 0
-    self.observed_data = ~np.isnan(feedback_matrix) # True values are observed ratings, False values are unobserved ratings
+    self.feedbacks = np.nan_to_num( np.array(feedbacks), 0 ) # replace NaNs with 0s
+    self.observed_data = ~np.isnan( feedbacks ) # True --> observed ratings, 
+                                                # False --> unobserved ratings
     
-    self.n_users, self.n_items = feedback_matrix.shape # number of users and items
-    self.n_iter = n_iter # number of iterations
-    self.n_latents = n_latents # number of latent factors
+    self.n_users, self.n_items = feedbacks.shape # get num. of users and items from the feedback matrix
+    self.n_iter = n_iter  # num. of iterations for the algorithm
+    self.n_latents = n_latents # num. of latent factors
     self.w_obs = w_obs # weight of observed 
     self.w_unobs = w_unobs # weight of unobserved 
-    self.lambda_reg = lambda_reg  # regularization parameter
+    self.lambda_reg = lambda_reg # regularization parameter
     
     if seed is not None: 
-      np.random.seed(seed)  # set the seed for reproducibility
+      np.random.seed(seed)  # seed for reproducibility
 
-    # initialize the user and item matrices with random values
-    self.user_matrix = np.random.rand(self.n_users, n_latents)  
-    self.item_matrix = np.random.rand(self.n_items, n_latents)  
+    # random values for the user and item matrices:
+    self.users_embedding = np.random.rand(self.n_users, n_latents) # n_users x n_latents
+    self.items_embedding = np.random.rand(self.n_items, n_latents) # n_items x n_latents
+
+    self.verbose = False
 
   def fit(self, method:str='WALS', verbose:str=False) -> dict:
     """
     Fit the model
     """
+
+    if verbose:
+      self.verbose = True
+
     print(f"** fitting the model with {method} method **")
-    start = time.process_time()
-    hist = {}
+    start = time.process_time()   # start timer
+    hist = {}                     # dict. for loss function values
 
     match method:
       case 'wals':
@@ -46,8 +54,8 @@ class WeightedMatrixFactorization():
       case _:
         raise NotImplementedError(f"Method {method} not implemented.")
       
-    end = time.process_time()
-    print(f"** done in {end-start:.2f} seconds **")
+    end = time.process_time()                       # end timer
+    print(f"** done in {end-start:.2f} seconds **") # print elapsed time
     return hist
   
   def __sgd_method(self, verbose) -> None:
@@ -60,28 +68,28 @@ class WeightedMatrixFactorization():
     """
     history = {} # to store the loss function values
 
-    for _ in range(self.n_iter):
+    for i in range(self.n_iter):
 
-      self.__update_user_matrix()
-      self.__update_item_matrix()
+      # UPDATE USER AND ITEM EMBEDDINGS:
+      self.__update_users_embedding()
+      self.__update_items_embedding()
 
-      # calculate the loss function value
+      # LOSS FUNCTION: 
       loss = np.sum(
         np.where(
-            self.observed_data,
-            (self.feedback_matrix - self.user_matrix @ self.item_matrix.T) ** 2,
+            self.observed_data, # apply the formula only to observed ratings
+            (self.feedbacks - self.users_embedding @ self.items_embedding.T) ** 2,
             0
         )
       )
 
-      history[_] = loss # store the loss function value
-
-      if verbose:
-        print(f"Loss: {loss:.3f}, iteration: {_+1}/{self.n_iter}")
+      history[i] = loss # store loss function value at iteration i
+      if self.verbose:
+        print(f"Loss: {loss:.3f}, iteration: {i+1}/{self.n_iter}")
 
     return history
 
-  def __update_user_matrix(self) -> None:
+  def __update_users_embedding(self) -> None:
     """
     Update the user matrix
     """
@@ -103,13 +111,13 @@ class WeightedMatrixFactorization():
       regularization = self.lambda_reg * np.eye(self.n_latents)
       
       # Solve the system of linear equations
-      self.user_matrix[user_idx,:] = np.linalg.solve(
-        self.item_matrix.T @ weight_matrix @ self.item_matrix + regularization, # n_latents x n_latents
-        self.item_matrix.T @ weight_matrix @ self.feedback_matrix[user_idx, :] # n_latents x 1
+      self.users_embedding[user_idx,:] = np.linalg.solve(
+        self.items_embedding.T @ weight_matrix @ self.items_embedding + regularization, # n_latents x n_latents
+        self.items_embedding.T @ weight_matrix @ self.feedbacks[user_idx, :] # n_latents x 1
       )
     return
   
-  def __update_item_matrix(self) -> None:
+  def __update_items_embedding(self) -> None:
     """
     Update the item matrix
     """
@@ -130,9 +138,9 @@ class WeightedMatrixFactorization():
       regularization = self.lambda_reg * np.eye(self.n_latents)
 
       # Solve the system of linear equations using spsolve
-      self.item_matrix[item_idx, :] = solve(
-        self.user_matrix.T @ weight_matrix @ self.user_matrix + regularization,
-        self.user_matrix.T @ weight_matrix @ self.feedback_matrix[:, item_idx]
+      self.items_embedding[item_idx, :] = solve(
+        self.users_embedding.T @ weight_matrix @ self.users_embedding + regularization,
+        self.users_embedding.T @ weight_matrix @ self.feedbacks[:, item_idx]
       )
     return
   
@@ -140,11 +148,19 @@ class WeightedMatrixFactorization():
     """
     Predict the rating for a given user and item
     """
-    return self.user_matrix[user_idx, :] @ self.item_matrix[item_idx, :].T
+    return self.users_embedding[user_idx, :] @ self.items_embedding[item_idx, :].T
 
   def predict_all(self) -> np.ndarray:
     """
     Predict the ratings for all users and items
     """
-    return self.user_matrix @ self.item_matrix.T
+    return self.users_embedding @ self.items_embedding.T
   
+  def save(self, path:str) -> None:
+    """
+    Save the model
+    """
+
+    with open(path, 'wb') as f:
+      pickle.dump(self, f)
+    return
